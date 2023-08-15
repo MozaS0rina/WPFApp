bd70fc29a4ff0de2ef0ee9148ad744c1083882e6
Sorina Moza <109519501+MozaS0rina@users.noreply.github.com>
2023-08-15 15:37:30 +0300

    #final_commit

---
+++ WpfAppPOS/WpfAppPOS/Fereastra_Servicii.xaml.cs
@@ -0,0 +1,68 @@
+﻿using app1;
+using entitati;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppPOS
+{
+    /// <summary>
+    /// Interaction logic for Fereastra_Servicii.xaml
+    /// </summary>
+    public partial class Fereastra_Servicii : Window
+    {
+        ServiciuMgr serviciuMgr = new ServiciuMgr();
+        public static List<Serviciu> lista_add = new List<Serviciu>();
+    
+        public Fereastra_Servicii()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Fereastra_servicii_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textbox_afisare_servicii_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        private void afisare_serviciinoi_Click(object sender, RoutedEventArgs e)
+        {
+            textbox_afisare_servicii.Text = null;
+            foreach (Serviciu serv in lista_add)
+                textbox_afisare_servicii.AppendText(serv.Descriere() + Environment.NewLine);
+        }
+
+        private void buton_adaugas_Click_1(object sender, RoutedEventArgs e)
+        {
+            Servicii_tastatura srv_add = new Servicii_tastatura();
+            srv_add.ShowDialog();
+            textbox_afisare_servicii.Text = null;
+            foreach (Serviciu serv in lista_add)
+                textbox_afisare_servicii.AppendText(serv.Descriere() + Environment.NewLine);
+        }
+
+        private void buton_afisares_Click_1(object sender, RoutedEventArgs e)
+        {
+            textbox_afisare_servicii.Text = null;
+            List<Serviciu> lista = serviciuMgr.InitListafromXML();
+            foreach (Serviciu serv in lista)
+                textbox_afisare_servicii.AppendText(serv.Descriere() + Environment.NewLine);
+        }
+    }
+}
---
+++ WpfAppPOS/WpfAppPOS/bin/Debug/ProduseMgr.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using entitati;
+
+
+namespace app1
+{
+    public class ProduseMgr : ProduseAbstractMgr
+    {
+        public List<Produs> InitListafromXML()
+        {
+           List<Produs> elem = new List<Produs>();//lista produse
+            int NrElemente = 0;
+
+            XmlDocument doc = new XmlDocument(); //initializam lista dintr-un fisier XML
+            doc.Load("Produse.xml");//incarcam fisierul       
+            XmlNodeList lista_noduri = doc.SelectNodes("/produse/Produs"); //selectam nodurile
+            foreach (XmlNode nod in lista_noduri)
+            {
+                //itereaza si selecteaza cimpurile fiecarui nod si
+                //informatia continuta in cadrul proprietatii InnerText
+                string nume = nod["Nume"].InnerText;
+                string codIntern = nod["CodIntern"].InnerText;
+                string producator = nod["Producator"].InnerText;
+                int pret = int.Parse(nod["Pret"].InnerText);
+                string categorie = nod["Categorie"].InnerText;
+
+                //adaugam in lista produse
+                elem.Add(new Produs(NrElemente + 1, nume, codIntern, producator, pret, categorie));
+                NrElemente++;
+            }
+            return elem;
+        }
+    }
+}
